1feb64eb3e4e75a5e6bfbf090ab954b0eb302652
PEDRO MACHADO DA SILVA <02339335060@senacrs.edu.br>
2025-08-14 15:50:41 -0300

    trabalho

--- trabalhoLordOfTheRings/src/routes/CharactersRoutes.ts
+++ trabalhoLordOfTheRings/src/routes/CharactersRoutes.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
+import express,{Router, NextFunction,Request,Response, Application} from 'express';
 import { CharacterController } from '../controllers/CharactersController';
 
 const router = Router();
 const controller = new CharacterController();
-
+const app:Application = express();
 
 
 router.get('/characters/:id',controller.getById)
@@ -11,5 +11,6 @@ router.get('/characters',controller.listAll);
 router.post('/characters',controller.create);
 router.delete('/characters/:id',controller.delete);
 router.put('/characters/:id',controller.update);
+
 
 export default router;
--- trabalhoLordOfTheRings/src/server.ts
+++ trabalhoLordOfTheRings/src/server.ts
@@ -1,20 +1,51 @@
-import express, { Application} from "express";
+import express, { Application, NextFunction, Request, Response, } from "express";
 import router from "./routes/CharactersRoutes";
-
-const app:Application = express()
+import { connection } from "./config/database";
+const app: Application = express()
 const PORT = 3000;
-
-
-
 app.use(express.json());
+
+
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+    if (req.method === "GET") {
+        
+        const [rows]:any[] = await connection.query(
+            "SELECT tipo FROM characters WHERE id = ?", [req.params.id]
+        ) 
+
+        if(rows.length > 0){
+            const tipo = rows[0].tipo;
+        switch (tipo) {
+            case "Sociedade":
+                console.log("Corram seus tolos!");
+                break;
+
+            case "Nazgûl":
+                console.log("Os Nazgûl não estão em Moria.");
+                break;
+
+            case "Balrog":
+                console.log("Você não vai passar!");
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+
+    next();
+});
+
+
+
 app.use(router)
 
+app.use((req, res) => {
+  res.status(404).json({erro:"A passagem de Caradhras está fechada por Saruman. Esta rota não existe para nós. Só nos sobrou...Moria."})
+})
 
-
-
-
-
-app.listen(PORT,() =>{
+app.listen(PORT, () => {
     console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
-    
+
 })
