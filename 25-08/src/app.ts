13ceede8ae8b7897d07c8eed5f24fe7603683867
PEDRO MACHADO DA SILVA <02339335060@senacrs.edu.br>
2025-08-27 17:15:23 -0300

    aula

--- 25-08/src/app.ts
+++ 25-08/src/app.ts
@@ -2,9 +2,16 @@
 import express from 'express'
 import { config } from 'dotenv'
 import routes from './routes'
+import cors from "cors"
+
+
+
+
 
 config()
 const app = express()
+//app.use(cors()) //permite requisição de QUALQUER ORIGEM
+app.use(cors({origin: "http://127.0.0.1:5500"}))
 app.use(express.json())
 app.use(routes)
 
